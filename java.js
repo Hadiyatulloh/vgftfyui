5ff9added57d79c8056e5028110fce189fbd2364
Hadiyatulloh <hadiyatullohfayzullayev@gmail.com>
2024-12-26 15:43:49 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+//  let person = {
+//      name: "xadado",
+//      age: 30,
+//      isStudent: false
+//  }
+//  let car = new object();
+//  car.brand = 'tesla';
+//  car.medel = 'model s';
+//  car.year = 2022;
+
+//  console.log(person.name);
+//  console.log(person.age);
+
+
+//  console.log(person["name"]);
+//  console.log(person["age"]);
+
+
+//  person.city = "New York";
+//  person.age(person["age"]);
+
+
+//  delete person.isStudent;
+let a = ["Olma", "Banan", "Gilos", "Shaftoli", ]
+console.log(a)
+alert(`Boshlangich Arrayning uzunligi ${a.length}`)
+alert('Lyuboy malumotni optashimiz')
+a.pop()
+console.log(a)
+
+alert(a.length)
